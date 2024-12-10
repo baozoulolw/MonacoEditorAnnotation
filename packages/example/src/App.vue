352e2e7e6142c12414632edd9358a39fe5701ad0
Baozoulolw <Baozoulolw@gmail.com>
2024-12-10 22:54:49 +0800

    fix : 自定义参数提取

--- packages/example/src/App.vue
+++ packages/example/src/App.vue
@@ -7,37 +7,93 @@ import { onMounted, useTemplateRef } from "vue";
 import * as monaco from "monaco-editor";
 import { register } from "monaco-editor-annotation";
 import { useTest } from "./utils";
+import traverse, { NodePath, Node } from "@babel/traverse";
+import * as t from "@babel/types";
 
 const editorDom = useTemplateRef("editor");
 
 const initEditor = () => {
   register(monaco);
   const editor = monaco.editor.create(editorDom.value!, {
-    value: `let a = {
-  methods:{
-    getRender(){
-    },
-    geta:function(){
-    },
-    async geth(){
-    },
-    getj(){
-      return '123'
-    }
-  }
-}`,
+    value: `let b = (a = window.onload,{n},[m],...c) => {}`,
     language: "javascript",
     theme: "vs-dark",
     automaticLayout: true,
   });
   console.log(editor);
   editor.addCommand(
-    monaco.KeyMod.WinCtrl | monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyM,
+    monaco.KeyMod.Shift | monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyM,
     () => {
-      const ast = useTest(editor); // 获取文档模型
-      console.log(ast);
+      const result = useTest(editor);
+      if (!result) return;
+      const { ast, index } = result;
+      traverse(ast, {
+        FunctionDeclaration(node) {
+          console.log("FunctionDeclaration", node);
+        },
+        FunctionExpression(node) {
+          console.log("FunctionExpression", node);
+        },
+        ObjectMethod(node) {
+          console.log("ObjectMethod", node, 2);
+        },
+        ArrowFunctionExpression(nodePath) {
+          const parentNode = nodePath.parentPath?.parent;
+          let startIndex = 0;
+          let endIndex = nodePath.node.end;
+          console.log(nodePath.node.params);
+          if (parentNode && parentNode.type === "VariableDeclaration") {
+            console.log(parentNode);
+            const { start, end } = parentNode.loc || {};
+            startIndex = start!.index || 0;
+          }
+        },
+      });
     }
   );
+
+  interface Param {
+    name: string;
+    desc: string;
+    defaultValue: any;
+    type: string;
+  }
+
+  const getParams = (params: Node[]) => {
+    let paramsArr: Param[] = [];
+    params.forEach((param) => {
+      const { type } = param;
+      // a = 123 有默认值
+      if (type === "AssignmentPattern") {
+        const { left, right } = param;
+        paramsArr.push({
+          name: left.name,
+          desc: "",
+          defaultValue: right.value,
+          type: right.type,
+        });
+      }
+      // {a,b} 解构
+      if (type === "ObjectPattern") {
+      }
+      // ...n 剩余参数
+      if (type === "RestElement") {
+      }
+    });
+  };
+
+  const getInfoByNode = (node: Node, params: Param[] = []): Param[] => {
+    const { type } = node;
+    if (type === "AssignmentPattern") {
+      const { left, right } = node;
+      params.push({
+        name: (left as t.Identifier).name,
+        desc: "",
+        defaultValue: right.value,
+        type: right.type,
+      });
+    }
+  };
 };
 onMounted(() => {
   initEditor();
--- packages/example/src/utils.ts
+++ packages/example/src/utils.ts
@@ -2,19 +2,27 @@ import { parse } from "@babel/parser";
 import { editor } from "monaco-editor";
 export const useTest = (
   editor: editor.IStandaloneCodeEditor
-): ReturnType<typeof parseContent> | null => {
+): {
+  ast: ReturnType<typeof parse>;
+  index: number;
+} | null => {
   const position = editor.getPosition()!; // 获取当前光标位置
   const model = editor.getModel()!;
   if (!model) return null;
-  let line = position.lineNumber;
-  let content = model.getLineContent(line);
-  let ast: ReturnType<typeof parseContent> | null = parseContent(content);
-  while (!ast && line < model.getLineCount()) {
-    debugger;
-    content += "\n" + model.getLineContent(++line);
-    ast = parseContent(content);
-  }
-  return ast;
+  const str = model.getValue();
+  const ast = parse(str);
+  return {
+    ast,
+    index: model.getOffsetAt(position),
+  };
+  // let content = model.getLineContent(line);
+  // let ast: ReturnType<typeof parseContent> | null = parseContent(content);
+  // while (!ast && line < model.getLineCount()) {
+  //   debugger;
+  //   content += "\n" + model.getLineContent(++line);
+  //   ast = parseContent(content);
+  // }
+  // return ast;
 };
 
 const parseContent = (content: string): ReturnType<typeof parse> | null => {
